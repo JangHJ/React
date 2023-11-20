e9151ffc78e92e5a95df2eb6ce92f6bac2a17a08
JangHJ <yg9316@swu.ac.kr>
2023-11-20 17:31:49 +0900

    Update Counter.jsx

--- Introduction/Counter.jsx
+++ Introduction/Counter.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Counter(props){
-    const [count, setCount] = useCount(0);
+    const [count, setCount] = useState(0);
+
+    //componentDidMount, componentDidUpdate와 비슷하게 작동
+    useEffect(() => {
+        //브라우저 API를 사용해서 document의 title을 업데이트
+        document.title = `You clicked ${count} time`;
+    });
 
     return (
         <div>
             <p>총 {count}번 클릭했습니다.</p>
-            <button onClick={() => setcount(count + 1)}>
+            <button onClick={() => setCount(count + 1)}>
                 클릭
             </button>
         </div>
